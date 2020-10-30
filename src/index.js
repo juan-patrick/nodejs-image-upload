6affd684c10f45f2d9645f6a72553244188fbe47
juan-patrick <juan-p.andrade@outlook.com>
2020-10-30 18:37:36 -0300

    Cors bug fix

--- src/index.js
+++ src/index.js
@@ -1,31 +1,34 @@
 require('dotenv').config();
 
 const express = require('express');
-const mongoose = require('mongoose');
+const { connect } = require('mongoose');
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
 
+const routes = require('./routes');
+
 const app = express();
 
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
+app.use(routes);
 
-mongoose.connect(process.env.MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+(async () => {
+  await connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
+})();
 
 app.use(
   '/files',
   express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
 );
-
-app.use(require('./routes'));
 
 app.listen(process.env.PORT || 3333, () => {
   console.log({
