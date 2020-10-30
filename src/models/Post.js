151d16ad95e79c8c227bea7919dbbe9075834ddd
juan-patrick <juan-p.andrade@outlook.com>
2020-10-30 17:49:04 -0300

    Add delete and save middlewares

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,4 +1,8 @@
 const { Schema, model } = require('mongoose');
+const aws = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
 
 const PostSchema = new Schema(
   {
@@ -11,5 +15,32 @@ const PostSchema = new Schema(
     timestamps: true,
   }
 );
+
+PostSchema.pre('save', function () {
+  if (!this.url) {
+    this.url = `${process.env.APP_URL}/files/${this.key}`;
+  }
+});
+
+PostSchema.pre('remove', function () {
+  if (process.env.STORAGE_TYPE === 's3') {
+    return s3
+      .deleteObject({
+        Bucket: process.env.BUCKET_NAME,
+        Key: this.key,
+      })
+      .promise()
+      .then((response) => {
+        console.log(response.status);
+      })
+      .catch((response) => {
+        console.log(response.status);
+      });
+  } else {
+    return promisify(fs.unlink)(
+      path.resolve(__dirname, '..', '..', 'tmp', 'uploads', this.key)
+    );
+  }
+});
 
 module.exports = model('Post', PostSchema);
