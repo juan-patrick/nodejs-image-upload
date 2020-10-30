08efdddfd7556d775065e2f7792432398896c1a2
juan-patrick <juan-p.andrade@outlook.com>
2020-10-30 17:20:38 -0300

    Defines Post Controller

--- src/controllers/PostController.js
+++ src/controllers/PostController.js
@@ -1,8 +1,28 @@
 const Post = require('../models/Post');
 
 module.exports = {
-  async index() {},
-  async show() {},
-  async store() {},
-  async delete() {},
+  async index(req, res) {
+    const posts = await Post.find();
+
+    return res.status(200).json(posts);
+  },
+  async store(req, res) {
+    const { originalname: name, size, key, location: url = '' } = req.file;
+
+    const post = await Post.create({
+      name,
+      size,
+      key,
+      url,
+    });
+
+    return res.status(201).json(post);
+  },
+  async delete(req, res) {
+    const post = await Post.findById(req.params.id);
+
+    await post.remove();
+
+    return res.status(200).json(post);
+  },
 };
